8ea765dab297ebacdc1f605a18d37400b595d530
higorhms <higorhmscebrac@hotmail.com>
2019-10-11 08:19:43 -0300

    Implementando o upload de imagens

---
+++ src/config/multer.js
@@ -0,0 +1,19 @@
+import crypto from 'crypto';
+import multer from 'multer';
+import { extname, resolve } from 'path';
+
+export default {
+    storage: multer.diskStorage({
+        destination: resolve(__dirname, '..', '..', 'temp', 'uploads'),
+        filename: (req, file, callBack) => {
+            crypto.randomBytes(16, (err, res) => {
+                if (err) return callBack(err);
+
+                return callBack(
+                    null,
+                    res.toString('hex') + extname(file.originalname)
+                );
+            });
+        },
+    }),
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -12,5 +15,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), (req, res) => {
+    res.json({ msg: 'ok' });
+});
 
 export default routes;
