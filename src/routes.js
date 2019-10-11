52cd62bf769f6625c5345a1c16e179dc01319dd1
higorhms <higorhmscebrac@hotmail.com>
2019-10-11 15:32:44 -0300

    Implementando a listagem dos providers

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -17,5 +18,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
 
 export default routes;
