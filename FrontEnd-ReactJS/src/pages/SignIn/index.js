6219ef4322f02d72a01387c9ab4d9be8b7f3636c
higorhms <higorhmscebrac@hotmail.com>
2019-12-16 14:00:47 -0300

    Criando as rotas da aplicação e configurando o history

---
+++ FrontEnd-ReactJS/src/pages/SignIn/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+    return (
+        <div>
+            <h1>SignIn</h1>
+        </div>
+    );
+}
