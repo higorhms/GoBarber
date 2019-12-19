5259fe37eca579047a66162abb4b5387cefbf517
higorhms <higorhmscebrac@hotmail.com>
2019-12-19 19:58:09 -0300

    Configurando o redux, redux-saga e o reactotron-redux-saga

--- FrontEnd-ReactJS/src/App.js
+++ FrontEnd-ReactJS/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 import history from './services/history';
 
+import store from './store';
+
 import GlobalStyle from './styles/global';
 
 function App() {
     return (
-        <Router history={history}>
-            <Routes />
-            <GlobalStyle />
-        </Router>
+        <Provider store={store}>
+            <Router history={history}>
+                <Routes />
+                <GlobalStyle />
+            </Router>
+        </Provider>
     );
 }
 
---
+++ FrontEnd-ReactJS/src/store/modules/auth/reducer.js
@@ -0,0 +1,8 @@
+const INITIAL_STATE = { token: '123' };
+
+export default function auth(state = INITIAL_STATE, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
