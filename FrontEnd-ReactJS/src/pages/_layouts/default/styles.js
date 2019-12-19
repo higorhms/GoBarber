7b5b5baffe93922c510d446d4e85c5a363779d0b
higorhms <higorhmscebrac@hotmail.com>
2019-12-18 21:07:18 -0300

    Finalizando os estilos globais da aplicação

---
+++ FrontEnd-ReactJS/src/pages/_layouts/default/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    height: 100%;
+    background: linear-gradient(-10deg, #301199, #100496);
+`;
--- FrontEnd-ReactJS/src/routes/Route.js
+++ FrontEnd-ReactJS/src/routes/Route.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import Proptypes from 'prop-types';
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
 
 export default function RouteWrapper({
     component: Component,
     isPrivate,
     ...rest
 }) {
-    const signed = false;
+    const signed = true;
 
     if (!signed && isPrivate) {
         return <Redirect to="/" />;
@@ -17,7 +19,18 @@ export default function RouteWrapper({
         return <Redirect to="/dashboard" />;
     }
 
-    return <Route {...rest} component={Component} />;
+    const Layout = signed ? AuthLayout : DefaultLayout;
+
+    return (
+        <Route
+            {...rest}
+            render={props => (
+                <Layout>
+                    <Component {...props} />
+                </Layout>
+            )}
+        />
+    );
 }
 
 RouteWrapper.propTypes = {
