1be3b27093846dc8e3fa059eacb12fe88bfa8c50
higorhms <higorhmscebrac@hotmail.com>
2019-12-21 11:18:18 -0300

    Finalizando o funcionamento do Dashboard

--- FrontEnd-ReactJS/src/pages/Dashboard/index.js
+++ FrontEnd-ReactJS/src/pages/Dashboard/index.js
@@ -10,6 +10,7 @@ import {
     isBefore,
     isEqual,
     parseISO,
+    setMilliseconds,
 } from 'date-fns';
 import { utcToZonedTime } from 'date-fns-tz';
 import pt from 'date-fns/locale/pt';
@@ -38,11 +39,13 @@ export default function Dashboard() {
             const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
             const data = range.map(hour => {
-                const checkDate = setSeconds(
-                    setMinutes(setHours(date, hour), 0),
+                const checkDate = setMilliseconds(
+                    setSeconds(setMinutes(setHours(date, hour), 0), 0),
                     0
                 );
+
                 const compareDate = utcToZonedTime(checkDate, timezone);
+
                 return {
                     time: `${hour}:00h`,
                     past: isBefore(compareDate, new Date()),
@@ -51,7 +54,6 @@ export default function Dashboard() {
                     ),
                 };
             });
-
             setSchedule(data);
         }
         loadSchedule();
