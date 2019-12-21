0f27c7571e04789eaf6d655d5f421aa03a8edbf6
higorhms <higorhmscebrac@hotmail.com>
2019-12-21 09:02:13 -0300

    Finalizando o funcionamento do Dashboard

--- FrontEnd-ReactJS/src/pages/Dashboard/index.js
+++ FrontEnd-ReactJS/src/pages/Dashboard/index.js
@@ -1,58 +1,97 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
-import { format, addDays, subDays } from 'date-fns';
+import {
+    format,
+    addDays,
+    subDays,
+    setHours,
+    setMinutes,
+    setSeconds,
+    isBefore,
+    isEqual,
+    parseISO,
+} from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 import pt from 'date-fns/locale/pt';
 
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
 export default function Dashboard() {
+    const [schedule, setSchedule] = useState([]);
     const [date, setDate] = useState(new Date());
-    const formattedDate = useMemo(
+
+    const dateFormatted = useMemo(
         () => format(date, "d 'de' MMMM", { locale: pt }),
         [date]
     );
 
-    function prevDay() {
+    useEffect(() => {
+        async function loadSchedule() {
+            const response = await api.get('schedules', {
+                params: { date },
+            });
+
+            const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+            const data = range.map(hour => {
+                const checkDate = setSeconds(
+                    setMinutes(setHours(date, hour), 0),
+                    0
+                );
+                const compareDate = utcToZonedTime(checkDate, timezone);
+                return {
+                    time: `${hour}:00h`,
+                    past: isBefore(compareDate, new Date()),
+                    appointment: response.data.find(a =>
+                        isEqual(parseISO(a.date), compareDate)
+                    ),
+                };
+            });
+
+            setSchedule(data);
+        }
+        loadSchedule();
+    }, [date]);
+
+    function handlePrevDay() {
         setDate(subDays(date, 1));
     }
 
-    function nextDay() {
+    function handleNextDay() {
         setDate(addDays(date, 1));
     }
 
     return (
         <Container>
             <header>
-                <button onClick={prevDay} type="button">
+                <button type="button" onClick={handlePrevDay}>
                     <MdChevronLeft size={36} color="#FFF" />
                 </button>
-
-                <strong>{formattedDate}</strong>
-
-                <button onClick={nextDay} type="button">
+                <strong>{dateFormatted}</strong>
+                <button type="button" onClick={handleNextDay}>
                     <MdChevronRight size={36} color="#FFF" />
                 </button>
             </header>
 
             <ul>
-                <Time past>
-                    <strong>08:00</strong>
-                    <span>Higor Martins</span>
-                </Time>
-                <Time available>
-                    <strong>09:00</strong>
-                    <span>Em aberto</span>
-                </Time>
-                <Time>
-                    <strong>10:00</strong>
-                    <span>Higor Martins</span>
-                </Time>
-                <Time>
-                    <strong>11:00</strong>
-                    <span>Higor Martins</span>
-                </Time>
+                {schedule.map(time => (
+                    <Time
+                        key={time.time}
+                        past={time.past}
+                        available={!time.appointment}
+                    >
+                        <strong>{time.time}</strong>
+                        <span>
+                            {time.appointment
+                                ? time.appointment.user.name
+                                : 'Em aberto'}
+                        </span>
+                    </Time>
+                ))}
             </ul>
         </Container>
     );
