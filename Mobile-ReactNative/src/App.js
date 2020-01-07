cf07c2096765df21675f9921153fcf4e30cdbe5a
higorhms <higorhmscebrac@hotmail.com>
2020-01-07 10:36:53 -0300

    Configurando as rotas do App

--- Mobile-ReactNative/src/App.js
+++ Mobile-ReactNative/src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import Routes from './routes';
 
 export default function App() {
-    return (
-        <View>
-            <Text>RN Structure</Text>
-        </View>
-    );
+    return <Routes />;
 }
