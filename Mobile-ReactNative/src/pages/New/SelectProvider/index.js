be9a820aeaeba85d3549c13e0e5e6f6bc22562e7
higorhms <higorhmscebrac@hotmail.com>
2020-01-27 22:08:40 -0300

    Pagina de providers

--- Mobile-ReactNative/src/pages/New/SelectProvider/index.js
+++ Mobile-ReactNative/src/pages/New/SelectProvider/index.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
+
+import api from '~/services/api';
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+export default function SelectProvider({ navigation }) {
+    const [providers, setProviders] = useState([]);
 
-export default function SelectProvider() {
-    return <Background />;
+    useEffect(() => {
+        async function loadProviders() {
+            const response = await api.get('/providers');
+            setProviders(response.data.providers);
+        }
+        loadProviders();
+    }, []);
+
+    return (
+        <Background>
+            <Container>
+                <ProvidersList
+                    data={providers}
+                    keyExtractor={provider => String(provider.id)}
+                    renderItem={({ item: provider }) => (
+                        <Provider
+                            onPress={() =>
+                                navigation.navigate('SelectDateTime', {
+                                    provider,
+                                })
+                            }
+                        >
+                            <Avatar
+                                source={{
+                                    uri:
+                                        (provider.avatar &&
+                                            provider.avatar.url) ||
+                                        `https://api.adorable.io/avatars/50/${provider.name}.png`,
+                                }}
+                            />
+                            <Name>{provider.name}</Name>
+                        </Provider>
+                    )}
+                />
+            </Container>
+        </Background>
+    );
 }
-
 SelectProvider.navigationOptions = ({ navigation }) => ({
     title: 'Selecione o prestador',
     headerLeft: () => (
@@ -17,7 +55,7 @@ SelectProvider.navigationOptions = ({ navigation }) => ({
                 navigation.navigate('Dashboard');
             }}
         >
-            <Icon name="chevron-left" size={20} color="#FFF" />
+            <Icon name="chevron-left" size={25} color="#fff" />
         </TouchableOpacity>
     ),
 });
