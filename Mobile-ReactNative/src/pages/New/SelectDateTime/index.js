ca8ac663722f43aae5570c6a8a45320feae8fdf3
higorhms <higorhmscebrac@hotmail.com>
2020-01-28 20:31:27 -0300

    Configurando o datepicker de cada plataforma

--- Mobile-ReactNative/src/pages/New/SelectDateTime/index.js
+++ Mobile-ReactNative/src/pages/New/SelectDateTime/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import Background from '~/components/Background';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
+import DateInput from '~/components/DateInput/index.android';
+import { Container } from './styles';
 
 export default function SelectDateTime() {
-    return <Background />;
+    const [date, setDate] = useState(new Date());
+
+    return (
+        <Background>
+            <Container>
+                <DateInput date={date} onChange={setDate} />
+            </Container>
+        </Background>
+    );
 }
 
-SelectDateTime.navigationOptions = {
+SelectDateTime.navigationOptions = ({ navigation }) => ({
     title: 'Selecione o horário',
-};
+    headerLeft: () => (
+        <TouchableOpacity
+            onPress={() => {
+                navigation.goBack();
+            }}
+        >
+            <Icon name="chevron-left" size={25} color="#fff" />
+        </TouchableOpacity>
+    ),
+});
