c0cc0236f1972b1d6b0ea22c842be5b79c757dac
higorhms <higorhmscebrac@hotmail.com>
2020-01-28 21:29:22 -0300

    Finalizando a funcionalidade de agendamento

--- Mobile-ReactNative/src/pages/New/Confirm/index.js
+++ Mobile-ReactNative/src/pages/New/Confirm/index.js
@@ -1,8 +1,62 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
 
-export default function Confirm() {
-    return <View />;
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+import api from '~/services/api';
+
+export default function Confirm({ navigation }) {
+    const provider = navigation.getParam('provider');
+    const time = navigation.getParam('time');
+
+    const dateFormatted = useMemo(
+        () => formatRelative(parseISO(time), new Date(), { locale: pt }),
+        [time]
+    );
+
+    async function handleAddAppointment() {
+        await api.post('appointments', {
+            provider_id: provider.id,
+            date: time,
+        });
+
+        navigation.navigate('Dashboard');
+    }
+
+    return (
+        <Background>
+            <Container>
+                <Avatar
+                    source={{
+                        uri:
+                            (provider.avatar && provider.avatar.url) ||
+                            `https://api.adorable.io/avatars/50/${provider.name}.png`,
+                    }}
+                />
+
+                <Name>{provider.name}</Name>
+                <Time>{dateFormatted}</Time>
+                <SubmitButton onPress={handleAddAppointment}>
+                    Confirmar Agendamento
+                </SubmitButton>
+            </Container>
+        </Background>
+    );
 }
+
+Confirm.navigationOptions = ({ navigation }) => ({
+    title: 'Confirmar agendamento',
+    headerLeft: () => (
+        <TouchableOpacity
+            onPress={() => {
+                navigation.goBack();
+            }}
+        >
+            <Icon name="chevron-left" size={25} color="#fff" />
+        </TouchableOpacity>
+    ),
+});
