35b28f42b254b280b35be2bf243f84d96a3cfb6a
higorhms <higorhmscebrac@hotmail.com>
2020-05-07 11:20:29 -0300

    Refactoring Appointment Model

--- GoBarber2.0/BACKEND-NodeJS/src/models/Appointment.ts
+++ GoBarber2.0/BACKEND-NodeJS/src/models/Appointment.ts
@@ -1,17 +1,15 @@
-import { uuid } from 'uuidv4';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn()
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('time with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
