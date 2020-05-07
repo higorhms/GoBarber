ee6f5ec0fb5ddb8daca87e6d071a5c05efa34ad5
higorhms <higorhmscebrac@hotmail.com>
2020-05-07 14:37:15 -0300

    Relation between models (Appointment and User)

--- GoBarber2.0/BACKEND-NodeJS/src/models/Appointment.ts
+++ GoBarber2.0/BACKEND-NodeJS/src/models/Appointment.ts
@@ -4,7 +4,10 @@ import {
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -12,7 +15,11 @@ class Appointment {
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('time with time zone')
   date: Date;
