7b0dd099ea1221ea55e8243038d45d8839ff21ba
higorhms <higorhmscebrac@hotmail.com>
2020-05-09 16:57:06 -0300

    Setting structure

---
+++ GoBarber2.0/FRONTEND-ReactJS/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
