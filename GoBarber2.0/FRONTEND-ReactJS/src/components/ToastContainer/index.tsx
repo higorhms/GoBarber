4a1327215316bab537d8003eaad2b249c8cd49ac
higorhms <higorhmscebrac@hotmail.com>
2020-05-13 17:16:47 -0300

    Finishing Toasts

--- GoBarber2.0/FRONTEND-ReactJS/src/components/ToastContainer/index.tsx
+++ GoBarber2.0/FRONTEND-ReactJS/src/components/ToastContainer/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
 
 import { ToastMessage } from '../../hooks/ToastContext';
 import { Container } from './styles';
@@ -9,10 +10,22 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransitions = useTransition(
+    messages,
+    (message) => message.id,
+    {
+      from: { right: '-120%', opacity: 0 },
+      enter: { right: '0%', opacity: 1 },
+      leave: { right: '-120%', opacity: 0 },
+    },
+  );
+
   return (
     <Container>
-      {messages &&
-        messages.map((message) => <Toast key={message.id} message={message} />)}
+      {messagesWithTransitions &&
+        messagesWithTransitions.map(({ item, key, props }) => (
+          <Toast key={key} style={props} message={item} />
+        ))}
     </Container>
   );
 };
