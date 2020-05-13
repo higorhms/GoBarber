109713d07db8777526b600bb8cc5d4c228ac9d4b
higorhms <higorhmscebrac@hotmail.com>
2020-05-13 18:13:47 -0300

    Authenticated routes

---
+++ GoBarber2.0/FRONTEND-ReactJS/src/routes/Route.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouteProps,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/AuthContext';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={() => {
+        return isPrivate === !!user ? (
+          <Component {...rest} />
+        ) : (
+          <Redirect to={{ pathname: isPrivate ? '/' : '/dashboard' }} />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
