725d779bfba7fc3eaa1f7fee308d7c5cae7fd423
higorhms <higorhmscebrac@hotmail.com>
2020-05-19 14:04:43 -0300

    Finishing authentication tests

--- GoBarber2.0/BACKEND-NodeJS/src/modules/users/services/AuthenticateUserService.spec.ts
+++ GoBarber2.0/BACKEND-NodeJS/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import AuthenticateUserService from './AuthenticateUserService';
 import CreateUsersService from './CreateUsersService';
@@ -29,5 +30,47 @@ describe('AuthenticateUser', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user.email).toBe('johndoe@johndoe.com');
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeHashProvider = new FakeHashProvider();
+    const usersRepository = new FakeUsersRepository();
+    const authenticateUser = new AuthenticateUserService(
+      usersRepository,
+      fakeHashProvider,
+    );
+    const usersService = new CreateUsersService(
+      usersRepository,
+      fakeHashProvider,
+    );
+
+    await usersService.execute({
+      name: 'johndoe',
+      email: 'johndoe@johndoe.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@johndoe.com',
+        password: 'wrong-password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate an unexisting user', async () => {
+    const fakeHashProvider = new FakeHashProvider();
+    const usersRepository = new FakeUsersRepository();
+    const authenticateUser = new AuthenticateUserService(
+      usersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@johndoe.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
