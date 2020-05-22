fdc5525613168d28de5c992cab8f02f1be73a6dc
higorhms <higorhmscebrac@hotmail.com>
2020-05-22 15:15:25 -0300

    Finishing email template

--- GoBarber2.0/BACKEND-NodeJS/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ GoBarber2.0/BACKEND-NodeJS/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import AppError from '@shared/errors/AppError';
+import path from 'path';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
@@ -31,6 +32,13 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -38,10 +46,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[Gobarber] Recuperação de senha',
       templateData: {
-        template: 'Olá {{name}}: {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
