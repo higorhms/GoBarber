e50c52fd624d3b662076860674fe2520c5932b24
higorhms <higorhmscebrac@hotmail.com>
2020-05-31 15:07:39 -0300

    Setting emails sending using Amazon SES

--- GoBarber2.0/BACKEND-NodeJS/src/shared/container/providers/index.ts
+++ GoBarber2.0/BACKEND-NodeJS/src/shared/container/providers/index.ts
@@ -1,11 +1,14 @@
 import { container } from 'tsyringe';
 
+import mailConfig from '@config/mail';
+
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
@@ -19,5 +22,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider),
 );
