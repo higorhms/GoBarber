977a966cdded95ba0e185d06aad39fde619d43eb
higorhms <higorhmscebrac@hotmail.com>
2020-06-11 12:28:49 -0300

    Setting routes config

--- GoBarber2.0/MOBILE-ReactNative/src/App.tsx
+++ GoBarber2.0/MOBILE-ReactNative/src/App.tsx
@@ -1,8 +1,20 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import AuthRoutes from './routes';
 
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+        <AuthRoutes />
+      </View>
+    </NavigationContainer>
+  );
 };
 
 export default App;
---
+++ GoBarber2.0/MOBILE-ReactNative/src/pages/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <View />
+    </Container>
+  );
+};
+
+export default SignIn;
