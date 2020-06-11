001ddb5e8dc3a03b5081b64e7e12bdf79b0a7d52
higorhms <higorhmscebrac@hotmail.com>
2020-06-11 16:53:46 -0300

    Working with Icons and setting Input and Button components

--- GoBarber2.0/MOBILE-ReactNative/src/pages/SignIn/index.tsx
+++ GoBarber2.0/MOBILE-ReactNative/src/pages/SignIn/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
@@ -10,6 +12,15 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button
+        onPress={() => {
+          console.log('funfo');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
