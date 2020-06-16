79f591cfc5a8d2c8aa7d230e5a73098413971938
higorhms <higorhmscebrac@hotmail.com>
2020-06-16 18:05:35 -0300

    Creating Header of CreateAppointment page

--- GoBarber2.0/MOBILE-ReactNative/src/pages/CreateAppointment/index.tsx
+++ GoBarber2.0/MOBILE-ReactNative/src/pages/CreateAppointment/index.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+import { useAuth } from '../../hooks/AuthContext';
+
+import * as S from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  return <View />;
+  const route = useRoute();
+  const { user } = useAuth();
+  const { goBack } = useNavigation();
+  const { providerId } = route.params as RouteParams;
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  return (
+    <S.Container>
+      <S.Header>
+        <S.BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </S.BackButton>
+
+        <S.HeaderTitle>Cabeleireiros</S.HeaderTitle>
+        <S.UserAvatar source={{ uri: user.avatar_url }} />
+      </S.Header>
+    </S.Container>
+  );
 };
 
 export default CreateAppointment;
--- GoBarber2.0/MOBILE-ReactNative/src/pages/Dashboard/index.tsx
+++ GoBarber2.0/MOBILE-ReactNative/src/pages/Dashboard/index.tsx
@@ -78,7 +78,13 @@ const Dashboard: React.FC = () => {
           <ProviderContainer
             onPress={() => navigateToCreateAppointment(provider.id)}
           >
-            <ProviderAvatar source={{ uri: provider.avatar_url }} />
+            <ProviderAvatar
+              source={{
+                uri:
+                  provider.avatar_url ||
+                  'https://api.adorable.io/avatars/101/abott@adorable.png',
+              }}
+            />
             <ProviderInfo>
               <ProviderName>{provider.name}</ProviderName>
 
