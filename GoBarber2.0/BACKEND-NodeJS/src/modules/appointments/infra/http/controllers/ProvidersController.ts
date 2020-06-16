bf3b105facc187e9d21b589fe33a4b50a830fdf2
higorhms <higorhmscebrac@hotmail.com>
2020-06-15 22:05:18 -0300

    Saving list of providers in cache on a right way

--- GoBarber2.0/BACKEND-NodeJS/src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ GoBarber2.0/BACKEND-NodeJS/src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 
@@ -11,7 +12,7 @@ class ProvidersController {
 
     const providers = await listProvidersService.execute({ user_id });
 
-    return response.json(providers);
+    return response.json(classToClass(providers));
   }
 }
 
--- GoBarber2.0/BACKEND-NodeJS/src/modules/appointments/services/ListProvidersService.ts
+++ GoBarber2.0/BACKEND-NodeJS/src/modules/appointments/services/ListProvidersService.ts
@@ -29,10 +29,13 @@ class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
-    return classToClass(users);
+    return users;
   }
 }
 
