3352b5bd97cc4b7df09021b97755bdfdc36581f2
higorhms <higorhmscebrac@hotmail.com>
2020-06-16 16:21:36 -0300

    Styling the list of providers on the dashboard

--- GoBarber2.0/MOBILE-ReactNative/src/pages/Dashboard/index.tsx
+++ GoBarber2.0/MOBILE-ReactNative/src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
 import { useNavigation } from '@react-navigation/native';
 import api from '../../services/api';
@@ -11,6 +12,13 @@ import {
   ProfileButton,
   UserAvatar,
   ProvidersList,
+  ProvidersListTitle,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderInfo,
+  ProviderName,
+  ProviderMeta,
+  ProviderMetaText,
 } from './styles';
 
 export interface Provider {
@@ -35,6 +43,13 @@ const Dashboard: React.FC = () => {
     signOut();
   }, [signOut, navigation]);
 
+  const navigateToCreateAppointment = useCallback(
+    (providerId: string) => {
+      navigation.navigate('CreateAppointment', { providerId });
+    },
+    [navigation],
+  );
+
   return (
     <Container>
       <Header>
@@ -56,7 +71,28 @@ const Dashboard: React.FC = () => {
       <ProvidersList
         data={providers}
         keyExtractor={(provider) => provider.id}
-        renderItem={({ item }) => <UserName>{item.name}</UserName>}
+        ListHeaderComponent={
+          <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+        }
+        renderItem={({ item: provider }) => (
+          <ProviderContainer
+            onPress={() => navigateToCreateAppointment(provider.id)}
+          >
+            <ProviderAvatar source={{ uri: provider.avatar_url }} />
+            <ProviderInfo>
+              <ProviderName>{provider.name}</ProviderName>
+
+              <ProviderMeta>
+                <Icon name="calendar" size={14} color="#ff9000" />
+                <ProviderMetaText>Segunda a Sexta</ProviderMetaText>
+              </ProviderMeta>
+              <ProviderMeta>
+                <Icon name="clock" size={14} color="#ff9000" />
+                <ProviderMetaText>8h às 18h</ProviderMetaText>
+              </ProviderMeta>
+            </ProviderInfo>
+          </ProviderContainer>
+        )}
       />
     </Container>
   );
--- GoBarber2.0/MOBILE-ReactNative/src/pages/Dashboard/styles.ts
+++ GoBarber2.0/MOBILE-ReactNative/src/pages/Dashboard/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 import { FlatList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 import { Provider } from '.';
 // import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
@@ -34,4 +35,51 @@ export const UserAvatar = styled.Image`
   border-radius: 28px;
 `;
 
-export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)``;
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px 16px;
+`;
+
+export const ProvidersListTitle = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  margin-bottom: 24px;
+  font-size: 24px;
+  color: #f4ede8;
+`;
+
+export const ProviderContainer = styled(RectButton)`
+  background: #3e3b47;
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 16px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 72px;
+  height: 72px;
+  border-radius: 36px;
+`;
+
+export const ProviderInfo = styled.View`
+  flex: 1;
+  margin-left: 20px;
+`;
+
+export const ProviderName = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 18px;
+  color: #f4ede8;
+`;
+
+export const ProviderMeta = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 8px;
+`;
+
+export const ProviderMetaText = styled.Text`
+  margin-left: 8px;
+  color: #999591;
+  font-family: 'RobotoSlab-Regular';
+`;
