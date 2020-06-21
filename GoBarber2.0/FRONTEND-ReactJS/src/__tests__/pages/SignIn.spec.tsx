1bd95a611d5a4d6b04bc4ee0a5baabd41ee5063f
higorhms <higorhmscebrac@hotmail.com>
2020-06-21 11:28:07 -0300

    Coverage Report

--- GoBarber2.0/FRONTEND-ReactJS/src/__tests__/pages/SignIn.spec.tsx
+++ GoBarber2.0/FRONTEND-ReactJS/src/__tests__/pages/SignIn.spec.tsx
@@ -1,9 +1,11 @@
-import { render } from '@testing-library/react';
 import React from 'react';
+import { render } from '@testing-library/react';
+
 import SignIn from '../../pages/SignIn';
 
 jest.mock('react-router-dom', () => {
   return {
+    useHistory: jest.fn(),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
--- GoBarber2.0/FRONTEND-ReactJS/src/pages/SignIn/index.tsx
+++ GoBarber2.0/FRONTEND-ReactJS/src/pages/SignIn/index.tsx
@@ -2,8 +2,7 @@ import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
-
+import { Link, useHistory } from 'react-router-dom';
 import { FormHandles } from '@unform/core';
 
 import Input from '../../components/Input';
@@ -27,57 +26,70 @@ interface SignInFormData {
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
   const { signIn } = useAuth();
   const { addToast } = useToast();
+  const history = useHistory();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
-      formRef.current?.setErrors({});
-
       try {
+        formRef.current?.setErrors({});
+
         const schema = Yup.object().shape({
-          email: Yup.string().required('Valid e-mail is required').email(),
-          password: Yup.string().required('Password is required'),
+          email: Yup.string()
+            .required('Email obrigatório')
+            .email('Digite um email válido'),
+          password: Yup.string().required('Senha obrigatória'),
         });
 
         await schema.validate(data, { abortEarly: false });
-        await signIn({ email: data.email, password: data.password });
-      } catch (error) {
-        if (error instanceof Yup.ValidationError) {
-          const errors = getValidationErrors(error);
+
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
+
+        history.push('/dashboard');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
           formRef.current?.setErrors(errors);
+
           return;
         }
         addToast({
           type: 'error',
           title: 'Erro na autenticação',
-          description: 'Por favor cheque as credenciais',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
         });
       }
     },
-    [signIn, addToast],
+    [signIn, addToast, history],
   );
 
   return (
     <Container>
       <Content>
         <AnimationContainer>
-          <img src={logo} alt="logo" />
-          <Form onSubmit={handleSubmit} ref={formRef}>
+          <img src={logo} alt="Go Barber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Faça seu logon</h1>
             <Input name="email" icon={FiMail} placeholder="E-mail" />
             <Input
               name="password"
               icon={FiLock}
               type="password"
-              placeholder="Password"
+              placeholder="Senha"
             />
+
             <Button type="submit">Entrar</Button>
-            <Link to="forgot-password">Esqueci minha senha</Link>
+            <Link to="/forgot-password">Esqueci minha senha</Link>
           </Form>
-          <Link to="signup">
+          <Link to="/signup">
             <FiLogIn />
-            Criar minha conta
+            Criar conta
           </Link>
         </AnimationContainer>
       </Content>
