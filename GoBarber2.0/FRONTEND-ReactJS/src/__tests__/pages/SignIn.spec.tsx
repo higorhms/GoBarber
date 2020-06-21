f6888b222fb0e39cd93e773d8fd7754aa1f445aa
higorhms <higorhmscebrac@hotmail.com>
2020-06-21 16:20:03 -0300

    More tests on SignIn page

--- GoBarber2.0/FRONTEND-ReactJS/src/__tests__/pages/SignIn.spec.tsx
+++ GoBarber2.0/FRONTEND-ReactJS/src/__tests__/pages/SignIn.spec.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, wait } from '@testing-library/react';
 
 import SignIn from '../../pages/SignIn';
 
+const mockedPushFunction = jest.fn();
+
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedPushFunction,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
+jest.mock('../../hooks/AuthContext', () => ({
+  useAuth: () => ({
+    signIn: jest.fn(),
+  }),
+}));
+
 describe('SignIn Page', () => {
-  it('should be able to sign in', () => {
+  it('should be able to render SignIn Page', () => {
     const { debug } = render(<SignIn />);
 
     debug();
+  });
+
+  it('should be able to fill the form and signin', async () => {
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: { value: 'teste@teste.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+    fireEvent.click(buttonElement);
+
+    await wait(() => {
+      expect(mockedPushFunction).toHaveBeenCalledWith('/dashboard');
+    });
   });
 });
