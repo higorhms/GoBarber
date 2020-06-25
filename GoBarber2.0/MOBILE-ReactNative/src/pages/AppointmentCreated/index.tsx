89c16d187e01677ca1e4b74e61066fd62c7ad86a
higorhms <higorhmscebrac@hotmail.com>
2020-06-25 11:53:27 -0300

    Creating AppointmentCreated page

--- GoBarber2.0/MOBILE-ReactNative/src/pages/AppointmentCreated/index.tsx
+++ GoBarber2.0/MOBILE-ReactNative/src/pages/AppointmentCreated/index.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
-// import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluido</Title>
+      <Description>Horario:</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
